b93ec15feacbe87d48356f168ca869d427497dac
andres aguirre <aguirregzz97@gmail.com>
2021-07-12 11:54:18 -0500

    🔧 Add swagger to project

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv-safe';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import { SwaggerUtils } from './utils/SwaggerUtils';
+
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.MY_NAME ?? 5000;
 
-app.use(express.json());
+const swaggerDocs = swaggerJsDoc(SwaggerUtils.swaggerOptions);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SwaggerUtils.swaggerOptions), express.json());
 
 app.get('/', (_, res) => {
   res.send('Hello World!');
